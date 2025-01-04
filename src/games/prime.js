206533430460606a10134aa6147be94a926103aa
Карина Демиденко <krndemidenko@gmail.com>
2025-01-04 18:24:27 +0300

    logic for prime game adjusted

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,11 +2,11 @@ import runGame from "../index.js";
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isPrime = (number) => {
-    if (number < 2) 
+const isPrime = (num) => {
+    if (num <= 1) 
         return false;
-    for (let i = 2; i < Math.sqrt(); i+= 1) {
-        if (number % i === 0)
+    for (let i = 2; i < Math.sqrt(num); i+= 1) {
+        if (num % i === 0)
             return false;
     }
     return true;
