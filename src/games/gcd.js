efa615f4378a9e582b4d079506c1568b6b0492d4
Карина Демиденко <krndemidenko@gmail.com>
2025-01-11 18:06:56 +0300

    new improvements

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,17 +4,17 @@ import generateRandomNumber from '../utils.js';
 const description = 'Find the greatest common divisor of given numbers.';
 
 const findGcd = (a, b) => {
-    let x = a;
-    let y = b;
+  let x = a;
+  let y = b;
   while (y !== 0) {
-    [x, y] = [y , x % y];
+    [x, y] = [y, x % y];
   }
   return x;
 };
 
 const generateRound = () => {
   const num1 = generateRandomNumber(1, 100);
-  const num2 = generateRandomNumber(1,100);
+  const num2 = generateRandomNumber(1, 100);
   const question = `${num1} ${num2}`;
   const correctAnswer = String(findGcd(num1, num2));
   return [question, correctAnswer];
