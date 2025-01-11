c0c127763be3dee5246f0a61314888189d94e101
Карина Демиденко <krndemidenko@gmail.com>
2025-01-11 16:44:50 +0300

    changes done

--- src/games/prime.js
+++ src/games/prime.js
@@ -6,7 +6,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (num) => {
     if (num <= 1) 
         return false;
-    for (let i = 2; i < Math.sqrt(num); i+= 1) {
+    for (let i = 2; i <= Math.sqrt(num); i+= 1) {
         if (num % i === 0)
             return false;
     }
@@ -17,5 +17,5 @@ const generateRound = () => {
     const correctAnswer = isPrime(question) ? 'yes' : 'no'; 
     return [question, correctAnswer];
   };
-  
+
   export default () => runGame(description, generateRound);
