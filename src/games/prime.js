b906d9562f826922dc0bfa67634200a2bfe9d85c
Карина Демиденко <krndemidenko@gmail.com>
2025-01-11 16:59:22 +0300

    Another improvement

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,9 @@ import { generateRandomNumber } from '../utils.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-    if (num <= 1) 
-        return false;
+    if (num <= 1) return false;
     for (let i = 2; i <= Math.sqrt(num); i+= 1) {
-        if (num % i === 0)
-            return false;
+        if (num % i === 0) return false;
     }
     return true;
 }
